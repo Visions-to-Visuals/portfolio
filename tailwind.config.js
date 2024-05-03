a778970c4afa711f4584dfc1c0e410f44ec8de6c
Adrean Cajigas <adrean.cajigas@gmail.com>
2024-05-02 21:34:42 -0700

    feat: Set up tailwind config and favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+
+      colors: {
+        white: "#FFFFFF",    
+        black: "#000000",   
+        orange: "#EC512B",  
+        yellow: "#F5BF49",   
+      },
+
+      screens: {
+        'monitor': {'min': '2000px'},       // 1600px and beyond
+        'laptop': {'max': '1440px'},        // 1250px up to 1440px
+        'laptop-s': {'max': '1250px'},      // 1024px up to 1250px
+        'tablet': {'max': '1024px'},        // 768px up to 1024px
+        'tablet-s': {'max': '768px'},       // 480px up to 768px
+        'mobile': {'max': '500px'},         // 340px up to 480px
+        'mobile-s': {'max': '340px'},       // 0px up to 340px
       },
     },
   },
