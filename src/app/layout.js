d36fad8717252a1ff33ab4f1dd6362126337a20f
Adrean Cajigas <adrean.cajigas@gmail.com>
2024-06-14 13:30:30 -0700

    fix: Hotjar integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,26 @@ export const metadata = {
   description: 'Welcome to Visions to Visuals, a professional web design and development company. We specialize in crafting stunning, user-friendly websites that elevate your brand’s digital presence and drive business growth. We understand that a website is more than just a digital brochure; it’s a powerful tool for engaging customers and enhancing your brand’s identity. We focus on delivering customized solutions that reflect your unique values and stories, ensuring an optimal user experience that boosts engagement and conversions. Contact us today to transform your vision into a visual reality.'
 };
 
+const HotjarSnippet = () => {
+  if (HOTJAR_ID) return null
+  return (
+    <div>
+      <Script id="hotjar-snippet">
+        {`
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:${HOTJAR_ID},hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+      </Script>
+    </div>
+  );
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" id="home" className="
@@ -16,6 +36,7 @@ export default function RootLayout({ children }) {
     mobile:text-[.9rem]
     mobile-s:text-[.85rem]
     ">
+      <HotjarSnippet></HotjarSnippet>
       <body>{children}</body>
     </html>
   );
